22389d816b68ee99900300e6a6d2dd0134f00cb3
KL <dev.lau3@gmail.com>
2020-04-01 15:48:06 -0500

    solved all 7 questions

--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -20,6 +20,34 @@ export default class App extends Component {
     this.axiosGetData=this.axiosGetData.bind(this);
     this.chainedRequestOne=this.chainedRequestOne.bind(this);
     this.chainedRequestTwo=this.chainedRequestTwo.bind(this);
+    this.simultaneousRequestsTwo=this.simultaneousRequestsTwo.bind(this);
+  }
+
+  simultaneousRequestsTwo(urls, idx, attr) {
+
+    //launch a few ajax request simultaneously using Promise.all
+    Promise.all(urls.map((url)=>
+      axios.get(url)
+      .then (response=> {
+        let resp=response.data;
+        console.log('2nd resp is in simultaneous mode--->', resp);
+  
+        let arr=this.state.answers;
+        // use attrElemIdx to find answer string if resp[attr] is array; otherwise answer is resp[attr] 
+        let attrValue=resp[attr];
+        if (idx in arr) {
+          arr[idx]+="; "+attrValue;   //append answer if one already exist
+        } else {
+          arr[idx]=attrValue;   //set the first answer
+        }
+        this.setState({answers: arr});   //setState wants to update the entire array, not just an element
+      })
+      .catch(error=>{
+        console.log('there is an error', error)
+      })
+
+    ), {idx: idx, attr: attr})  //used 2nd parm (object) to pass in additionl parms
+
   }
 
   chainedRequestTwo(url, idx, attr) {
@@ -41,6 +69,7 @@ export default class App extends Component {
     })
   }
 
+
   chainedRequestOne(url, idx, attr, nextAttr) { //first request of a chain of 2 requests
     //idx is question #, attr is response field containing anwser  
     //nextAttr is the response field in the next request that will contain answer
@@ -50,8 +79,14 @@ export default class App extends Component {
       let resp=response.data;
       console.log('1st resp of chain--->', resp);
 
-      let nextURL=resp[attr]; //expecting an URL
-      this.chainedRequestTwo(nextURL, idx, nextAttr);
+      let nextURL=resp[attr]; //expecting an URL or array of URL
+      
+      //Use simulaneous requests if an array of URLs is returned
+      if (Array.isArray(nextURL)) {
+        this.simultaneousRequestsTwo(nextURL, idx, nextAttr)
+      } else {
+        this.chainedRequestTwo(nextURL, idx, nextAttr);
+      }
     })
     .catch(error=>{
       console.log('there is an error', error)
@@ -65,34 +100,42 @@ export default class App extends Component {
     //chainedRequest causes a cascade of requests to find answer
     
     if (chainedRequest) {
+
+      //It is a chained requests. Start with request one
       this.chainedRequestOne(url, idx, attr, nextAttr);
+
+    } else {
+
+      //It is a singular request
+      axios.get(url)
+      .then (response=> {
+        let resp=response.data;
+        console.log('question-answer--->', resp);
+  
+        let arr=this.state.answers;
+        // use attrElemIdx to find answer string if resp[attr] is array; otherwise answer is resp[attr] 
+        let attrValue=Array.isArray(resp[attr]) ? resp[attr][attrElemIdx] : resp[attr];
+        arr[idx]=attrValue;   
+        this.setState({answers: arr});   //setState wants to update the entire array, not just an element
+  
+      })
+      .catch(error=>{
+        console.log('there is an error', error)
+      })
     }
 
-    axios.get(url)
-    .then (response=> {
-      let resp=response.data;
-      console.log('question-answer--->', resp);
-
-      let arr=this.state.answers;
-      // use attrElemIdx to find answer string if resp[attr] is array; otherwise answer is resp[attr] 
-      let attrValue=Array.isArray(resp[attr]) ? resp[attr][attrElemIdx] : resp[attr];
-      arr[idx]=attrValue;   
-      this.setState({answers: arr});   //setState wants to update the entire array, not just an element
-
-    })
-    .catch(error=>{
-      console.log('there is an error', error)
-    })
   }
 
   componentDidMount() {
+
+    //Lanuch requests for all 7 questions
     this.axiosGetData(q1URL, 1, 'born');
     this.axiosGetData(q2URL, 2, 'region');
     this.axiosGetData(q3URL, 3, 'coatOfArms');
     this.axiosGetData(q4URL, 4, 'seats', 1);   //get 2nd elem of an array
     this.axiosGetData(q5URL, 5, 'aliases', 1); //get 2nd elem of an array
     this.axiosGetData(q6URL, 6, 'founder', 0, true, 'name'); //chain to one more get request, last parm is the attr name for 2nd response that contains answer
-    this.axiosGetData(q7URL, 7, 'born');
+    this.axiosGetData(q7URL, 7, 'povBooks', 0, true, 'name');
   }
 
   render() {
@@ -111,9 +154,8 @@ export default class App extends Component {
             <h2>Ans: {this.state.answers[5]}</h2>
             <h2>Q6. What's the name of the founder of House Stark? (You have to chain fetch requests!)</h2>
             <h2>Ans: {this.state.answers[6]}</h2>
-            <h2>Q7. What are the titles of Catelyn Stark's three POV books? (Look into Promise.all to request these simultaniously)
-</h2>
-            <h2>Ans: {this.state.answers[1]}</h2>
+            <h2>Q7. What are the titles of Catelyn Stark's three POV books? (Look into Promise.all to request these simultaniously)</h2>
+            <h2>Ans: {this.state.answers[7]}</h2>
 
       </div>
     );
